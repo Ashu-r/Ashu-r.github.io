6c1aeeb19d9933f2665d0997f98b9d809f02a7f6
ashu-r <randiveashutosh@gmail.com>
2022-09-01 20:28:18 +0530

    Added and improved experience section

--- script/index.js
+++ script/index.js
@@ -34,3 +34,51 @@ window.addEventListener('scroll', async () => {
 	}
 	scrollValue = scrollY;
 });
+async function fetchData() {
+	let data = {};
+	await fetch('/data/data.json')
+		.then((res) => res.json())
+		.then((d) => {
+			data = d;
+		});
+	// about
+	const about = document.querySelector('#aboutData');
+	about.innerHTML = data.about;
+	// experience
+	const experience = document.querySelector('.experience');
+	for (let i = 0; i < data.experience.length; i++) {
+		let item = data.experience[i];
+		const expDiv = experience.cloneNode(true);
+		if (i === 0) {
+			expDiv.querySelector('.line-1').classList.add('bg-1dark');
+		} else if (i === data.experience.length - 1) {
+			expDiv.querySelector('.line-2').classList.add('bg-1dark');
+		}
+		console.log(i, data.experience.length - 1);
+		expDiv.classList.remove('d-none');
+		expDiv.querySelector(
+			'.exp-timeline'
+		).innerHTML = `${item.startDate} - ${item.endDate}`;
+		expDiv.querySelector('.exp-location').innerHTML = item.location;
+		expDiv.querySelector('.exp-description').innerHTML = item.description;
+		expDiv.querySelector('.exp-org').innerHTML = item.organization;
+		expDiv.querySelector('.exp-title').innerHTML = item.title;
+
+		experience.parentElement.appendChild(expDiv);
+	}
+	// education
+	const education = document.querySelector('.education');
+	for (let i = 0; i < data.education.length; i++) {
+		let item = data.education[i];
+		const eduDiv = education.cloneNode(true);
+		if (i === 0) {
+			eduDiv.querySelector('.line-1').classList.add('bg-1dark');
+		} else if (i === data.education.length - 1) {
+			eduDiv.querySelector('.line-2').classList.add('bg-1dark');
+		}
+		eduDiv.classList.remove('d-none');
+
+		education.parentElement.appendChild(eduDiv);
+	}
+}
+fetchData();
